90b9c6eb3f4a6d7859c840a05753bdda872c3ab6
hase218 <chju23041@g.nihon-u.ac.jp>
2024-10-11 23:45:14 +0900

    Main関数名の変更 Footerの作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import ImageSideBar from "./ImageSideBar";
 import WelcomeTxt from "./WelcomTxt";
 
-export default function Header() {
+export default function Main() {
     return (
         <main>
             <WelcomeTxt />
